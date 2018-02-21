de50db6fc9c28ee95d10af515636e0569694a22c
progettoingesoftware <34620499+progettoingesoftware@users.noreply.github.com>
2018-02-21 13:09:53 +0100

    Add files via upload

---
+++ Sottocategoria.java
@@ -0,0 +1,9 @@
+package it.ing.sw;
+
+public class Sottocategoria extends Categoria
+{
+	public String nome;
+	//Soluzione per non ereditare l'attributo elencoSottocategoria: dichiarare tale attributo private o package
+}
+
+
