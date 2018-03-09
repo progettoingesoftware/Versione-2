c1dc3bd23ef7b66dedd414d7023810a199d5a89f
progettoingesoftware <34620499+progettoingesoftware@users.noreply.github.com>
2018-03-09 22:51:34 +0100

    Delete Sottocategoria.java

--- Sottocategoria.java
+++
@@ -1,9 +0,0 @@
-package it.ing.sw;
-
-public class Sottocategoria extends Categoria
-{
-	public String nome;
-	//Soluzione per non ereditare l'attributo elencoSottocategoria: dichiarare tale attributo private o package
-}
-
-
